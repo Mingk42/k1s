0c8efd943fddf14def7d6486dc22933f05fd8230
Mingk42 <xoals123456t@gmail.com>
2024-10-31 17:03:47 +0900

    0.3

--- auto_scailer/src/auto_scailer/auto_scailer.py
+++
@@ -1,5 +0,0 @@
-from utils import get_limit
-from time import time
-
-scale_in_value, scale_out_value=get_limit()
-
--- auto_scailer/src/auto_scailer/timet.py
+++
@@ -1,8 +0,0 @@
-import time
-
-t=time.time()
-
-while True:
-    if t+5==time.time():
-        print(5)
-        t=time.time()
